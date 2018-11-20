eb0939d1135249acdadaab3b0e53a7e03856c0bc
aivantg <aivant.developer@gmail.com>
2018-11-20 16:51:45 -0600

    added rails-admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # Create all routes related to messages
   resources :messages, only: [:index, :show]
 

fff5fe23bcb0932c7e52ddaa7e97ec1d8653b314
Tony Yang <tonyyanga@gmail.com>
2018-11-27 13:53:49 -0800

    further go fmt utils.go

--- go/utils.go
+++ go/utils.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-    "math/rand"
-    "net/http"
+	"math/rand"
+	"net/http"
 )
 
 func getRandomKey() string {
-    // get a random key with length 32
-    bytes := make([]byte, 32)
-    for i := 0; i < 32; i++ {
-        bytes[i] = byte(65 + rand.Intn(25))  //A=65 and Z = 65+25
-    }
-    return string(bytes)
+	// get a random key with length 32
+	bytes := make([]byte, 32)
+	for i := 0; i < 32; i++ {
+		bytes[i] = byte(65 + rand.Intn(25)) //A=65 and Z = 65+25
+	}
+	return string(bytes)
 }
 
 func writeError(w http.ResponseWriter, msg string) {
-    http.Error(w, msg, 500)
+	http.Error(w, msg, 500)
 }
 
 func writeAccessDenied(w http.ResponseWriter, msg string) {
-    http.Error(w, msg, 404)
+	http.Error(w, msg, 404)
 }
-
-

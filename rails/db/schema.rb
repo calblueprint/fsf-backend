db68f260ddef7f74829505d3cee78c0bd0ca0ec7
Christopher Franco <francotorium@gmail.com>
2018-12-03 19:06:26 -0800

    Corrected schema conflict

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2018_12_02_005009) do
     t.boolean "news_alert", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "title"
   end
 
   create_table "petitions", force: :cascade do |t|

4007a9adbdfd67fada1710416f760fdfb3a84f3c
Fang <dfs.arthur@berkeley.edu>
2019-02-24 06:20:04 +0000

    Add a working unit test for store CC

---
+++ go/cas_server_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+// Set environment variables: TCUSERNAME="your_tc_username" TCPASSWORD="your_tc_password"
+
+// Test /payment/register endpoint
+func TestHandleRegisterCC(t *testing.T) {
+	tcUsername = os.Getenv("TCUSERNAME")
+	tcPassword = os.Getenv("TCPASSWORD")
+	prePayload := []byte(`{"name": "Delete Me After Test", "cc": "4111111111111111", "exp": "0223", "zip": "90000", "demo": "y"}`)
+	payLoad := bytes.NewBuffer(prePayload)
+
+	request, err := http.NewRequest("POST", "/payment/register", payLoad)
+
+	if err == nil {
+		handler := http.HandlerFunc(handleRegisterCC)
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, request)
+
+		if response.Code != 200 {
+			t.Errorf("/payment/register FAILED")
+		}
+	} else {
+		t.Errorf(err.Error())
+	}
+}

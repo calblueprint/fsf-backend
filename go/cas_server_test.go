13a87749fffd0dee5fedf3c6e7c687ff031ce2f5
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2019-02-23 22:10:30 -0800

    working version

--- go/cas_server_test.go
+++ go/cas_server_test.go
@@ -4,15 +4,15 @@ import (
 	"bytes"
 	"net/http"
 	"net/http/httptest"
-	"testing"
 	"os"
+	"testing"
 )
 
 // Test /payment/register endpoint
-func TestHandleRegisterCC(t *testing.T) { // it needs to begin with Test
-	tcUsername = os.Getenv("USERNAME")
-	tcPassword = os.Getenv("PW")
-	prePayload := []byte(`{"name": "Delete Me After Test", "cc": "4111111111111111", "exp": "0223", "zip": "90000"}`)
+func TestHandleRegisterCC(t *testing.T) {
+	tcUsername = os.Getenv("TCUSERNAME")
+	tcPassword = os.Getenv("TCPASSWORD")
+	prePayload := []byte(`{"name": "Delete Me After Test", "cc": "4111111111111111", "exp": "0223", "zip": "90000", "demo": "y"}`)
 	payLoad := bytes.NewBuffer(prePayload)
 
 	request, err := http.NewRequest("POST", "/payment/register", payLoad)
@@ -29,4 +29,3 @@ func TestHandleRegisterCC(t *testing.T) { // it needs to begin with Test
 		t.Errorf(err.Error())
 	}
 }
-

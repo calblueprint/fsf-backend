6d37c22f60fbcb91de444d9a50381cd139350cf1
Fang Shuo Deng <dfs.arthur@berkeley.edu>
2019-04-24 10:43:30 -0700

    working backend

--- rails/app/controllers/api/v1/messages_controller.rb
+++ rails/app/controllers/api/v1/messages_controller.rb
@@ -2,19 +2,20 @@ module Api
     module V1
         class MessagesController < ApplicationController
             def get_latest_messages
-
-
-                # Get the stringified parameter passed in from the end point
-                # un-stringify deadtime
-                two_hours_ago = DateTime.now - (5.0/24) # dummy value
-                last_sent = two_hours_ago
-
-                # filter Message.where('updated_at' > something)
-
-                # messages = Message.order('updated_at DESC').last(1);
-
-                messages = Message.where(updated_at: last_sent..DateTime.now).order('updated_at DESC')
-                render json: {status: 'SUCCESS', message:'Loaded FSF Messages', data: messages}, status: :ok
+                if(params.has_key?(:last_sent))
+                    # Get the stringified parameter passed in from the end point
+                    last_sent_param = params['last_sent']
+                    
+                    # parse time
+                    last_sent = Time.parse(last_sent_param)
+                    
+                    # filter Message.where('updated_at' > something)
+                    messages = Message.where(updated_at: last_sent..DateTime.now).order('updated_at DESC')
+                    # messages = Message.order('updated_at DESC').last(1);
+                    render json: {status: 'SUCCESS', message:'Loaded FSF Messages', data: messages}, status: :ok
+                else
+                    render json: {status: 'FAILURE', message:'Please pass in a last_sent parameter', data: {}}, status: :bad_request
+                end
             end
         end
     end
